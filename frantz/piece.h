fda8ca2abc51061eedcf1c713d8c0b0781b41bd0
frantzline93 <frantzline93@gmail.com>
2013-12-15 04:19:48 +0100

    robots

---
+++ frantz/piece.h
@@ -0,0 +1,16 @@
+/*
+* piece.h
+*
+* Created on: 14 déc. 2013
+* Author: kevin
+*/
+
+#ifndef PIECE_H_
+#define PIECE_H_
+
+struct Piece {
+        int numProduit;
+        int etat;
+};
+
+#endif /* PIECE_H_ */
---
+++ frantz/robot.h
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "piece.h"
+
+
+void Robot_1(int nb1,int tp1,*oper1){
+printf("Je suis le Robot n°1 ");
+nb1=3;
+tp1=1;
+
+
+}
+
+void Robot_2(int nb2,int tp2,*oper2){
+
+}
+void Robot_3(int nb3,int tp3,*oper3){
+
+}
+void Robot_4(int nb1,int tp1,*oper4){
+
+}
+void Robot_5(int nb1,int tp1,*oper1){
+
+}
+void Robot_6(int nb6,int tp6,*oper6){
+
+}
