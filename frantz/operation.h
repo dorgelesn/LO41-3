2006b53801645db03126c101ab6ab16c47503582
kevin <kevin@kevin-VirtualBox>
2013-12-15 05:31:47 +0100

    mise a jour de operation.h

--- frantz/operation.h
+++ frantz/operation.h
@@ -5,4 +5,14 @@
  *      Author: kevin
  */
 
+#ifndef OPERATION_H_
+#define OPERATION_H_
 
+struct Piece op1(struct Piece ressource[]);
+struct Piece op2(struct Piece ressource[]);
+struct Piece op3(struct Piece ressource[]);
+struct Piece op4(struct Piece ressource[]);
+struct Piece op5(struct Piece ressource[]);
+struct Piece op6(struct Piece ressource[]);
+
+#endif /* OPERATION_H_ */
